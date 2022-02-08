0a04b3d76be636834c111bb098022990081f91b9
gkiori <gkastane@gmail.com>
2022-02-08 19:28:08 +0100

    Affichage de la documentation.

--- src/main/java/fr/isika/cda15/projet1/annuaire/PanelUserDoc.java
+++ src/main/java/fr/isika/cda15/projet1/annuaire/PanelUserDoc.java
@@ -3,7 +3,6 @@ package fr.isika.cda15.projet1.annuaire;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,14 +14,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
-import javafx.scene.Cursor;
 //import javafx.scene.web.HTMLEditor;
 //import javafx.scene.web.WebEngine;
 //import javafx.scene.web.WebView;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -30,21 +27,7 @@ public class PanelUserDoc extends BorderPane {
 	
 	private static final String PATH_RESOURCES = "src/main/resources/";
 	private static Stage popUpDoc;
-	static double orgCliskSceneX;
-	static double orgReleaseSceneX;
-	static List<String> list = new ArrayList<String>();
-    static int j = 0;
-    static Button lbutton;
-	static Button rButton;
-    static ImageView imageView;
-	
-	static EventHandler<MouseEvent> circleOnMousePressedEventHandler = new EventHandler<MouseEvent>() {
-
-	    @Override
-	    public void handle(MouseEvent t) {
-	        orgCliskSceneX = t.getSceneX();
-	    }
-	};
+	private static int pageToShow = 0;
 	
 	public static void PanelUserDoc() {
 		
@@ -67,83 +50,97 @@ public class PanelUserDoc extends BorderPane {
 	private static BorderPane docBorder() {
 		
 		BorderPane principalBorder = new BorderPane();
-//		Image image1 = new Image (new FileInputStream(PATH_RESOURCES + "Documentation.png"));
-//		ImageView img1 = new ImageView();
-//		img1.setImage(image1);
-//		img1.setFitHeight(1024);
-//		img1.setFitWidth(700);
-//		principalBorder.setCenter(img1);
+//		VBox vbDoc = new VBox();
+//		vbDoc.setOnScroll(null);
+//		vbDoc.getChildren().add(img1);
+//		principalBorder.setCenter(vbDoc);
 		return principalBorder;
 	}
 	
-	private static void addUIControls (BorderPane principalBorber) throws IOException {
+	private static void addUIControls (BorderPane principalBorber) throws IOException  {
 		
-		try {
+		Image image1 = new Image (new FileInputStream(PATH_RESOURCES + "Documentation1.png"));
+		Image image2 = new Image (new FileInputStream(PATH_RESOURCES + "Documentation2.png"));
+		Image image3 = new Image (new FileInputStream(PATH_RESOURCES + "Documentation3.png"));
+		ImageView img1 = new ImageView();
+		img1.setImage(image1);
+		img1.setFitHeight(1024);
+		img1.setFitWidth(700);
+		
+		ImageView img2 = new ImageView();
+		img2.setImage(image2);
+		img2.setFitHeight(1024);
+		img2.setFitWidth(700);
+		
+		ImageView img3 = new ImageView();
+		img3.setImage(image3);
+		img3.setFitHeight(1024);
+		img3.setFitWidth(700);
+		
+		List<ImageView> listImage = new ArrayList<ImageView>();
+		listImage.add(img1);
+		listImage.add(img2);
+		listImage.add(img3);
+		
+		Button btnOK = new Button("Suivant");
+		Button btnBack = new Button("Précédent");
+		
+		btnOK.setOnAction(new EventHandler<ActionEvent>() {
 			
-			for (int i = 0; i < 3; i++) {
-				list.add(PATH_RESOURCES + "Documentation" + (i+1) + ".png");
+			@Override
+			public void handle(ActionEvent event) {
+				switch (pageToShow) {
+				case 0:
+					principalBorber.setCenter(listImage.get(pageToShow));
+					btnBack.setDisable(true);
+					pageToShow++;
+					break;
+				case 1:
+					principalBorber.setCenter(listImage.get(pageToShow));
+					btnBack.setDisable(false);
+					pageToShow++;
+					break;
+				case 2:
+					principalBorber.setCenter(listImage.get(pageToShow));
+					btnOK.setDisable(true);
+					btnBack.setDisable(false);
+					break;
+				}
+//				popUpDoc.close();	
 			}
+		});
+		
+		btnBack.setOnAction(new EventHandler<ActionEvent>() {
 			
-			System.out.println(list);
-
-	        GridPane gridPane = new GridPane();
-	        gridPane.setAlignment(Pos.CENTER);
-
-	        lbutton = new Button("<");
-	        rButton = new Button(">");
-
-	        Image images[] = new Image[list.size()];
-	        for (int i = 0; i < list.size(); i++) {
-	        	System.out.println("fichier " + i + ": " + list.get(i));
-	            images[i] = new Image(list.get(i));
-	        }
-
-	        imageView = new ImageView(images[j]);
-	        imageView.setCursor(Cursor.CLOSED_HAND);
-
-	        imageView.setOnMousePressed(circleOnMousePressedEventHandler);
-
-	        imageView.setOnMouseReleased(e -> {
-	            orgReleaseSceneX = e.getSceneX();
-	            if (orgCliskSceneX > orgReleaseSceneX) {
-	                lbutton.fire();
-	            } else {
-	                rButton.fire();
-	            }
-	        });
-
-	        rButton.setOnAction(e -> {
-	            j = j + 1;
-	            if (j == list.size()) {
-	                j = 0;
-	            }
-	            imageView.setImage(images[j]);
-
-	        });
-	        
-	        lbutton.setOnAction(e -> {
-	            j = j - 1;
-	            if (j == 0 || j > list.size() + 1 || j == -1) {
-	                j = list.size() - 1;
-	            }
-	            imageView.setImage(images[j]);
-
-	        });
-
-	        imageView.setFitHeight(1024);
-	        imageView.setFitWidth(700);
-
-	        HBox hBox = new HBox();
-	        hBox.setSpacing(15);
-	        hBox.setAlignment(Pos.CENTER);
-	        hBox.getChildren().addAll(imageView);
-
-	        gridPane.add(hBox, 1, 1);
-	        principalBorber.setCenter(gridPane);
-	        
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }	
-	}	
-}
+			@Override
+			public void handle(ActionEvent event) {
+				switch (pageToShow) {
+				case 0:
+					principalBorber.setCenter(listImage.get(pageToShow));
+					btnBack.setDisable(true);
+					btnOK.setDisable(false);
+					break;
+				case 1:
+					principalBorber.setCenter(listImage.get(pageToShow));
+					btnOK.setDisable(false);
+					pageToShow--;
+					break;
+				case 2:
+					principalBorber.setCenter(listImage.get(pageToShow));
+					btnOK.setDisable(false);
+					pageToShow--;
+					break;
+				}
+//				popUpDoc.close();	
+			}
+		});
+		GridPane gridPane = new GridPane();
+//		gridPane.getChildren().addAll(btnBack,btnOK);
+		gridPane.add(btnOK, 1, 0);
+		gridPane.add(btnBack, 0, 0);
+		gridPane.setAlignment(Pos.CENTER);
+		principalBorber.setCenter(listImage.get(pageToShow));
+		principalBorber.setTop(gridPane);
+	}
 	
+}
