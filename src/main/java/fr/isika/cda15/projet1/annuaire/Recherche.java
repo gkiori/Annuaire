a2687e31aa00575061e77be99ba093a045a447bb
parme <parme@DESKTOP-8URAQSL.home>
2022-02-08 19:45:37 +0100

    Ajout de commentaire

--- src/main/java/fr/isika/cda15/projet1/annuaire/Recherche.java
+++ src/main/java/fr/isika/cda15/projet1/annuaire/Recherche.java
@@ -1,16 +1,19 @@
 package fr.isika.cda15.projet1.annuaire;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeSet;
 
 public class Recherche {
 	
 // ******************* Méthode pour le choix du type de recherche  *******************
 
+	/**
+	 *  Méthode permettant de transformer le type du critère de la recherche (nom, prénom, département, année d'entrée ou promotion) en entier
+	 *  
+	 *  @param attributeType	String contenant le nom du critère recherché
+	 *  @return 				entier correspondant au critère recherché
+	 */
 	private static int keyValue(String attributeType) {
 		if(attributeType.equalsIgnoreCase("nom")) {
 			return 1;
@@ -35,6 +38,14 @@ public class Recherche {
 		}
 	}
 	
+	/**
+	 *  Méthode recherchant tout les stagiaires de la liste reçue en paramètre contenant la valeur du critère également reçu
+	 *  
+	 *  @param cle			String contenu l'élément recherché
+	 *  @param type			entier représentant le type de l'élément recherché
+	 *  @param setEntrant	HashSet contenant tout les stagiaires dont on vérifiera s'ils valident le critère
+	 *  @return 			retourne un HashSet contenant tout les stagiaire correspondant au critère reçu en paramètre
+	 */
 	public static HashSet<Stagiaire> rechercher(String cle, int type, HashSet<Stagiaire> setEntrant) {
 		HashSet<Stagiaire> setResultat = new HashSet<Stagiaire>();
 		switch (type) {
@@ -81,6 +92,13 @@ public class Recherche {
 	
 //************************ Méthodes de recherche multicritère ************************
 	
+	/**
+	 *  Méthode permettant de traiter une map de critère afin de préparer les appels nécessaires à la fonction rechercher
+	 *  
+	 *  @see 				Recherche#rechercher(String, int, HashSet)
+	 *  @listeRecherche		Map contenant tout les critères de recherche selectionnés par l'utilisateur
+	 *  @return 			retourne un HashSet contenant tout les stagiaires correspondant à tout les critères reçus en paramètre
+	 */
 	public static HashSet<Stagiaire> chercherMultiCle(Map<String, String> listeRecherche) {
 		HashSet<Stagiaire> resultatRecherche = new HashSet<Stagiaire>();
 		resultatRecherche = ArbreStagiaire.parcoursStagiaire();
@@ -100,6 +118,9 @@ public class Recherche {
 		return resultatRecherche;
 	}
 	
+	/**
+	 *  @return 	Retourne un TreeSet contenant toutes les différentes promotions existant dans le fichier
+	 */
 	public static TreeSet<String> getListePromo() {
 		HashSet<Stagiaire> stagiaires = ArbreStagiaire.parcoursStagiaire();
 		TreeSet<String> listePromo = new TreeSet<String>();
@@ -109,6 +130,9 @@ public class Recherche {
 		return listePromo;
 	}
 	
+	/**
+	 *  @return 	Retourne un TreeSet contenant tout les différents départements existant dans le fichier
+	 */
 	public static TreeSet<String> getListeDepartement() {
 		HashSet<Stagiaire> stagiaires = ArbreStagiaire.parcoursStagiaire();
 		TreeSet<String> listeDepartement = new TreeSet<String>();
@@ -118,6 +142,9 @@ public class Recherche {
 		return listeDepartement;
 	}
 	
+	/**
+	 *  @return 	Retourne un TreeSet contenant toutes les différentes années d'entrées existant dans le fichier
+	 */
 	public static TreeSet<String> getListeAnneeEntree() {
 		HashSet<Stagiaire> stagiaires = ArbreStagiaire.parcoursStagiaire();
 		TreeSet<String> listeAnneeEntree = new TreeSet<String>();
